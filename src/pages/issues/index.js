53c9c1f822a9228e64e5c9d4fc3a375bbb372888
Jason Miani <cswarrior1@g.ucla.edu>
2019-12-25 23:22:19 +0000

    fix

--- src/pages/issues/index.js
+++ src/pages/issues/index.js
@@ -8,47 +8,52 @@ function Index(props) {
     const [data, setData] = useState({});
     const [loading, setLoading] = useState("");
 
-    const dataPayload = {
+    const dataPayload = (data) => {
+        JSON.parse({
             "item": {
                 "payload": {
-                "name": JSON.stringify(data.name),
-                "email": JSON.stringify(data.emailAddress),
-                "subject": JSON.stringify(data.subject),
-                "type": JSON.stringify(data.type),
-                "description": JSON.stringify(data.description)
-            }}
-        }    
+                    "name": JSON.stringify(data.name),
+                    "email": JSON.stringify(data.emailAddress),
+                    "subject": JSON.stringify(data.subject),
+                    "type": JSON.stringify(data.type),
+                    "description": JSON.stringify(data.description)
+                }
+            }
+            
+    })}
 
-    function axiosPost(dataPayload) {
-
-    
-    setLoading("Loading...Hopefully this works!");
-    axios.post("https://send.pageclip.co/LBrR9bjbQI31kZlraPoWT9L3N2Rh9D41/IssueSubmissions", dataPayload)
-    .then(console.log(AxiosResponse), setLoading("Successful! Yeeeeeeeeet!"));
-}
-
-    return (
+    function axiosPost(data) {
+        setLoading("Loading...Hopefully this works!");
+        dataPayload(data);
+        axios.post("https://send.pageclip.co/LBrR9bjbQI31kZlraPoWT9L3N2Rh9D41/IssueSubmissions", dataPayload)
+            .then(console.log(AxiosResponse), setLoading("Successful! Yeeeeeeeeet!"));
+    }
+return (
         <Layout>
             <section className="section">
-                <form onSubmit={axiosPost} className="pageclip-form">
-            <input type="text" name="name" value={data.name} />
-            <input type="email" name="email" value={data.emailAddress} />
-            <select name="issue">
-                <option value={data.type.bug}>Bug</option>
-                <option value={data.type.feature}>Feature Request</option>
-                <option value={data.type.custom}>Custom</option>
-            </select>
-            <input type="text" name="subject" value={data.subject} />
-            <textarea name="description" value={data.description}>Description</textarea>
-            {loading}
-            <button type="submit" className="pageclip-form__submit">
-                <span>Send</span>
-            </button>
-        </form>
+                <div className="container">
+                    <div className="content">
+                        <form onSubmit={axiosPost} className="pageclip-form">
+                            <input type="text" name="name" value={setData(data.name)} />
+                            <input type="email" name="email" value={setData(data.emailAddress)} />
+                            <select name="issue">
+                                <option value={setData(data.type.bug)}>Bug</option>
+                                <option value={setData(data.type.feature)}>Feature Request</option>
+                                <option value={setData(data.type.custom)}>Custom</option>
+                            </select>
+                            <input type="text" name="subject" value={setData(data.subject)} />
+                            <textarea name="description" value={setData(data.description)}>Description</textarea>
+                            {loading}
+                            <button type="submit" className="pageclip-form__submit">
+                                <span>Send</span>
+                            </button>
+                        </form>
+                    </div>
+                </div>
             </section>
 
         </Layout>
-        
+
     );
 
 }
